5a75042780e2cef3220b301ff3de644a5388121e
jverhelst <jverhelst@ncompass-inc.com>
2017-09-21 08:53:18 -0500

    Database & connection

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'dbcollect',
+    user: process.env.DB_USER || 'dbcollect',
+    password: process.env.DB_PASS || 'dbcollect',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.DB_HOST || 'localhost',
+      storage: './dbcollect.sqlite'
+    }
+  }
+}
